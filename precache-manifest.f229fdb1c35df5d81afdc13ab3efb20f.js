d0f1fa6799f4a29a46b0b91bc32511fa8e053ff3
Ethan Parks <erparks24@gmail.com>
2019-01-30 12:45:38 -0700

    Updates

---
+++ precache-manifest.f229fdb1c35df5d81afdc13ab3efb20f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4640667703cdc5f97dd4",
+    "url": "/CSCI5849/static/css/main.2497f67f.chunk.css"
+  },
+  {
+    "revision": "4640667703cdc5f97dd4",
+    "url": "/CSCI5849/static/js/main.46406677.chunk.js"
+  },
+  {
+    "revision": "67b336dbbed62c153f38",
+    "url": "/CSCI5849/static/js/1.67b336db.chunk.js"
+  },
+  {
+    "revision": "c1a8e49bec62442ec2b2",
+    "url": "/CSCI5849/static/js/runtime~main.c1a8e49b.js"
+  },
+  {
+    "revision": "5877d2c20dfb2dd90b8644a74c39b503",
+    "url": "/CSCI5849/index.html"
+  }
+];
